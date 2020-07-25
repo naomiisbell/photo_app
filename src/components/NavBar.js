efc1ea14a3ea9f3ab35ebb09a2710333ab97c7a1
Naomi Isbell <naomiisbell@gmail.com>
2020-07-25 13:33:30 -0400

    created NavBar and Post components

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class NavBar extends React.Component {
+    render() {
+        return (
+            <div>
+            <div className="appHeader">
+                <h1 className="appHeaderName">Nomigram</h1>
+            </div>
+            </div>
+        )
+    }
+}
+
+export default NavBar;
