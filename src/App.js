ebe568db4a6c7ded44e3c85da066ffe7cd949576
Naomi Isbell <naomiisbell@gmail.com>
2020-07-25 13:54:13 -0400

    updated Post component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
+import Post from './components/Post'
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       {/* NavBar/header */}
+      <Post />
       {/* Pictures */}
     </div>
   );
