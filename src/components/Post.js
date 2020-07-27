4f2ae44889314a80f60aaddaa9af9499c4d99aa7
Naomi Isbell <naomiisbell@gmail.com>
2020-07-26 22:36:42 -0400

    added buttons for the post

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import Avatar from "@material-ui/core/Avatar"
+import { FaRegHeart } from 'react-icons/fa'
+import { FaRegComment } from 'react-icons/fa'
+import { FaRegBookmark } from 'react-icons/fa'
 
 class Post extends React.Component {
     render() {
@@ -14,6 +17,11 @@ class Post extends React.Component {
                 {/* header has username and photo */}
                 <img className="postImg" src="https://api.timeforkids.com/wp-content/uploads/2019/09/final-cover-forest.jpg"></img>
                 {/* image posted */}
+                <div className="buttons">
+                <button className="likeBtn"><FaRegHeart /></button>
+                <button className="commentBtn"><FaRegComment /></button>
+                <button className="bookmarkBtn"><FaRegBookmark /></button>
+                </div>
                 <h4 className="postText"><b>Username:</b> caption</h4>
                 {/* username and caption */}
             </div>
