e4dcdaad2f6b512a1f402b244216faac75fb3fd1
Naomi Isbell <naomiisbell@gmail.com>
2020-07-27 13:50:16 -0400

    changing buttons to font awesome

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import Avatar from "@material-ui/core/Avatar"
-import { FaRegHeart } from 'react-icons/fa'
-import { FaRegComment } from 'react-icons/fa'
-import { FaRegBookmark } from 'react-icons/fa'
+
 
 class Post extends React.Component {
     constructor(){
@@ -16,24 +14,27 @@ class Post extends React.Component {
         this.setState({
             btnClicked: true
         })
+        console.log('clicked')
     }
 
     render() {
         return (
             <div className="post">
+                <div className="postHeader">
                 <Avatar 
                     className="postAvatar"
                     alt="Naomi.Isbell"
                     src="static/images/1.jpg"
                 />
                 <h3>Username</h3>
+                </div>
                 {/* header has username and photo */}
                 <img className="postImg" src="https://api.timeforkids.com/wp-content/uploads/2019/09/final-cover-forest.jpg"></img>
                 {/* image posted */}
                 <div className="buttons">
-                <button onClick= {this.clickButton}className="likeBtn"><FaRegHeart size='2rem'/></button>
-                <button onClick= {this.clickButton} className="commentBtn"><FaRegComment size='2rem' /></button>
-                <button onClick= {this.clickButton}className="bookmarkBtn"><FaRegBookmark size='2rem'/></button>
+                <button onClick= {this.clickButton}>like</button>
+                <button onClick= {this.clickButton}>comment</button>
+                <button onClick= {this.clickButton}>save</button>
                 </div>
                 <h4 className="postText"><b>Username:</b> caption</h4>
                 {/* username and caption */}
