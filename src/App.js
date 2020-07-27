f7eabf6984639ec1a8c8695e551d3c658cdd033f
Naomi Isbell <naomiisbell@gmail.com>
2020-07-27 16:45:58 -0400

    added posts and signup page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
 import Post from './components/Post'
+import Post2 from './components/Post2'
+import Post3 from './components/Post3'
+import SignUpPage from './components/SignUpPage'
 
 function App() {
   return (
     <div className="App">
+      <SignUpPage />
       <NavBar />
       {/* NavBar/header */}
       <Post />
-      <Post />
+      <Post2 />
+      <Post3 />
       {/* Pictures */}
     </div>
   );
