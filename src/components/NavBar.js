742b764c2e4fbe2d96a67347685b9bd131fe58da
Naomi Isbell <naomiisbell@gmail.com>
2020-07-27 19:41:55 -0400

    uncommented my nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,15 +5,13 @@ class NavBar extends React.Component {
     render() {
         return (
             <div className="navBar">
-            <div className="header">
             <h1 className="headerName">Nomigram</h1>
-                {/* <nav>
+                <nav>
                     <li>Home</li>
                     <li>Profile</li>
                     <li>Bookmarks</li>
                     <li>Login</li>
-                </nav> */}
-            </div>
+                </nav>
             </div>
         )
     }
