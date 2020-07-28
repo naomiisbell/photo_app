02d6525c82a6d44472729ccf0d347d63fc014aa4
Naomi Isbell <naomiisbell@gmail.com>
2020-07-27 22:03:20 -0400

    profile pic and data

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,10 +1,40 @@
 import React from 'react'
+import { Avatar } from '@material-ui/core'
 
 class Profile extends React.Component {
     render() {
+        const avatarImageStyle = {
+            width: 200,
+            height: 200,
+        };
         return (
             <div>
-                
+                <h2>blm_movement</h2>
+                <div className="profilePage">
+                <Avatar 
+                className="profilePic"
+                style={Object.assign({}, avatarImageStyle, {color: 'grey' })}
+                src="https://i.redd.it/m5ackmclri351.jpg"
+                />
+                <div className="table">
+                    <table>
+                        <th>1</th>
+                        <th>155k</th>
+                        <th>115</th>
+                        <tr>
+                            <td>
+                                <p>Posts</p>
+                            </td>
+                            <td>
+                                <p>Followers</p>
+                            </td>
+                            <td>
+                                <p>Following</p>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
             </div>
         )
     }
