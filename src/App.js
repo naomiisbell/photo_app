7b3081e20fc674eaff832a486d9a0a30fe73f6d1
Naomi Isbell <naomiisbell@gmail.com>
2020-07-27 22:31:36 -0400

    added comments

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Post2 from './components/Post2'
 import Post3 from './components/Post3'
 import Profile from './components/Profile'
 import SignUpPage from './components/SignUpPage'
+import Bookmarks from './components/Bookmarks'
 import { FiHome } from 'react-icons/fi'
 import Avatar from "@material-ui/core/Avatar"
 import { FaRegBookmark } from 'react-icons/fa'
@@ -18,12 +19,14 @@ import { FiLogOut } from 'react-icons/fi'
 
 export default function App() {
   return (
+    // used react router to link my navbar to certain components
     <Router>
       <div className="navBar">
             <h1 className="headerName">Nomigram</h1>
                 <nav>
                     <button>
                       <li>
+                        {/* this links to the home page where all the users posts are located */}
                         <Link to="/post"><FiHome size="2rem" /></Link>
                       </li>
                     </button>
@@ -31,6 +34,7 @@ export default function App() {
 
                     <button>
                       <li>
+                        {/* this links to the profile page that we are on which is @blm_movement */}
                         <Link to="/profile"><Avatar src="https://i.redd.it/m5ackmclri351.jpg"/></Link>
                       </li>
                     </button>
@@ -38,6 +42,7 @@ export default function App() {
 
                     <button>
                       <li>
+                        {/* this links to my saved photos it does not have functionality */}
                         <Link to="/saved"><FaRegBookmark size="2rem"/></Link>
                       </li>
                     </button>
@@ -45,6 +50,7 @@ export default function App() {
 
                     <button>
                       <li>
+                        {/* this links to my sign in component */}
                         <Link to="/sign-in"><FiLogOut size="2rem"/></Link>
                       </li>
                     </button>
@@ -62,9 +68,9 @@ export default function App() {
           <Route path="/profile">
             <Profile />
           </Route>
-          {/* <Route path="/saved">
-            <Bookmarked />
-          </Route> */}
+          <Route path="/saved">
+            <Bookmarks />
+          </Route>
           <Route path="/sign-in">
             <SignUpPage />
           </Route>
