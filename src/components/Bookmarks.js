66c292c8f3e633027c72cd029827508e282a7c50
Naomi Isbell <naomiisbell@gmail.com>
2020-07-27 22:29:47 -0400

    added bookmark component

---
+++ src/components/Bookmarks.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Bookmarks extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>You have nothing bookmarked.</p>
+            </div>
+        )
+    }
+}
+
+export default Bookmarks;
